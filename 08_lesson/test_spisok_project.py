d47512b2c9e8d891c1d3c32389795ccea3f29be5
Nastia <galashova@list.ru>
2024-10-11 15:08:25 +0300

    Test

--- 08_lesson/test_spisok_project.py
+++ 08_lesson/test_spisok_project.py
@@ -1,14 +1,17 @@
 import requests
 
 
-
 base_url = "https://yougile.com"
+
+## Авторизация
+my_login = ''
+my_password = ''
 
 # Получить список компаний
 def test_auth():
     creds = {
-        'login': 'yellowratty@rustyload.com',
-        'password': '2335108'
+        'login': my_login,
+        'password': my_password
     }
     resp = requests.post(base_url + '/api-v2/auth/companies', json=creds)
     response_body = resp.json()["content"][0]
@@ -17,12 +20,11 @@ def test_auth():
     assert len(response_body) > 0
     return first_company
 
-
 # Создать ключ
 def test_key():
      creds = {
-        'login': 'yellowratty@rustyload.com',
-        'password': '2335108',
+        'login': my_login,
+        'password': my_password,
         'companyId': test_auth()
      }
      resp = requests.post(base_url + '/api-v2/auth/keys', json=creds)
@@ -30,30 +32,26 @@ def test_key():
      assert resp.status_code == 201
      return response_body
 
-
 # получить токен 
-
 def test_token():
     my_token = {
         'Authorization': ("Bearer " + test_key())
     }
     return my_token
 
-# Получить список проектов (надо исп полученный ключ в запросе)
-
-def test_get_project():
-    resp = requests.get(base_url+'/api-v2/projects', headers=test_token())
-    assert resp.status_code == 200
-
 # Получить ИД сотрудника
 def test_ID_worker():
      resp = requests.get(base_url+'/api-v2/users', headers=test_token())
      response_body = resp.json()["content"][0]
      worker = response_body["id"]
-     #assert worker == "b4c03f3c-badf-44f6-8206-cd7b2f236bea"
      assert resp.status_code == 200
      return worker
 
+## Позитивные проверки
+# Получить список проектов (надо исп полученный ключ в запросе)
+def test_get_project():
+    resp = requests.get(base_url+'/api-v2/projects', headers=test_token())
+    assert resp.status_code == 200
 
 # создать проект (надо получить ид сотрудника) 
 def test_new_project():
@@ -66,9 +64,7 @@ def test_new_project():
     resp = requests.post(base_url+'/api-v2/projects', json=project, headers=test_token())
     response_body = resp.json()["id"]
     assert resp.status_code == 201
-    assert resp.status_code == 401 
     return response_body
-
 
 # Получить по ID проект
 def test_id_project():
@@ -78,13 +74,42 @@ def test_id_project():
     assert id_project == response_body
     assert resp.status_code == 200
 
-
 # изменить проект 
 def test_change_project():
     id_project = test_new_project()
-    change = {
+    creds = {
         "title": "Очень новый проект"
         }
-    resp = requests.put(base_url+'/api-v2/projects/'+id_project, json=change, headers=test_token())
+    resp = requests.put(base_url+'/api-v2/projects/'+id_project, json=creds, headers=test_token())
     assert resp.status_code == 200
 
+
+## Негативные проверки
+# Получить список проектов без авторизации
+def test_noget_project():
+    resp = requests.get(base_url+'/api-v2/projects')
+    assert resp.status_code == 401
+
+# создать проект без ИД сотрудника
+def test_nonew_project():
+    project = {
+        "title": "Новый проект",
+        "users": {
+             test_ID_worker(): ""
+        }
+    }
+    resp = requests.post(base_url+'/api-v2/projects', json=project, headers=test_token())
+    assert resp.status_code == 400
+
+# Получить проект по несуществующему ID 
+def test_id_project():
+    resp = requests.get(base_url+'/api-v2/projects/'+"4567899", headers=test_token())
+    assert resp.status_code == 404
+
+# изменить не существующий проект 
+def test_change_project():
+    creds = {
+        "title": "Очень новый проект"
+        }
+    resp = requests.put(base_url+'/api-v2/projects/'+"4567899", json=creds, headers=test_token())
+    assert resp.status_code == 404
